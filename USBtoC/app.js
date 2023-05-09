8bba0deeced4f36bfa8e61debd0324dce4b9f381
camilozv21 <camilozv21@gmail.com>
2023-05-09 18:30:01 -0500

    delete folder made for Agus, move to folder usbtoc

---
+++ USBtoC/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}🚀`)); //Windows punto 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
